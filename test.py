c7b0bd81d43d8fa52a11d945f0c76bf85f79d21e
Esneider23 <98894964+Esneider23@users.noreply.github.com>
2022-11-24 20:08:41 -0500

    test

---
+++ test.py
@@ -0,0 +1,18 @@
+import unittest
+import requests
+
+
+class TestAppi(unittest.TestCase):
+    URL = "https://servicio-stock.onrender.com/vehiculos"
+
+    def test_1_get_all(self):
+        resp = requests.get(self.URL)
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(len(resp.json()), 2)
+        print("Test 1 completed")
+
+
+if __name__ == 'main':
+    tester = TestAppi()
+
+    tester.test_1_get_all()
