43739408332f3051af70faf4a5b8657ba5cfb0f7
Esneider23 <98894964+Esneider23@users.noreply.github.com>
2022-11-24 22:32:39 -0500

    update

--- test.py
+++ test.py
@@ -1,19 +1,15 @@
 import unittest
-from app import app
+
+
+def suma(a,b):
+    return a+b
 
 
 class BasicTest(unittest.TestCase):
-    def text_index(self):
-        tester = app.test_client(self)
-        resp = tester.get('https://servicio-stock.onrender.com/vehiculos')
-        code = resp.status_code
-        self.assertEqual(code, 200)
+    def test1(self):
+        c = suma(5, 10)
+        self.assertEqual(c, 15)
 
 
-<<<<<<< HEAD
 if __name__ == '_main_':
     unittest.main()
-=======
-if __name__ == 'main':
-    unittest.main()
->>>>>>> 996502837e8a0fe1c7475db6d436939eee51252d
