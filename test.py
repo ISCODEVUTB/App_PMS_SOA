678783fd9252442284c35350f7b7c7bf3704959f
Esneider23 <98894964+Esneider23@users.noreply.github.com>
2022-11-24 23:08:29 -0500

    update test

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
 import unittest
-import requests
 
 
 def suma(a, b):
@@ -11,11 +10,6 @@ class BasicTest(unittest.TestCase):
     def test1(self):
         c = suma(5, 10)
         self.assertEqual(c, 15)
-
-    def app_prueba(self):
-        URL = 'https://servicio-stock.onrender.com/vehiculo'
-        r = requests.get(URL)
-        self.assertEqual(r.status_code, 200)
 
 
 if __name__ == '_main_':
