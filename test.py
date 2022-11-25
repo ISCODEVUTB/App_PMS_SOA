8316086745b4ac7e487bb876aa6a3cb323271330
Esneider23 <98894964+Esneider23@users.noreply.github.com>
2022-11-24 22:59:10 -0500

    update test

--- test.py
+++ test.py
@@ -1,19 +1,23 @@
 import unittest
+from app import app
 
 
-def suma(a,b):
+def suma(a, b):
     return a+b
 
 
 class BasicTest(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
     def test1(self):
         c = suma(5, 10)
         self.assertEqual(c, 15)
 
+    def app_prueba(self):
+        r = self.app.get('https://servicio-stock.onrender.com/vehiculo', follow_redirects=True)
+        self.assertEqual(r.status_code, 200)
 
-<<<<<<< HEAD
+
 if __name__ == '_main_':
-=======
-if __name__ == 'main':
->>>>>>> 6d7eac1b75fa2862abc9a95ba0071ba11da54862
     unittest.main()
