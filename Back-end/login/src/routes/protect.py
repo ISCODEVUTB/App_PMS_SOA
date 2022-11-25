8671828c86d165a31f5c7ffd561e3b14e74e9e29
Esneider23 <98894964+Esneider23@users.noreply.github.com>
2022-11-24 19:16:25 -0500

    login

---
+++ Back-end/login/src/routes/protect.py
@@ -0,0 +1,39 @@
+from flask import Blueprint, request, jsonify
+from function_jwt import validate_token
+from database import mysql
+
+protect = Blueprint("protect", __name__)
+
+
+def token_info():
+    token = request.headers['Authorization'].split(" ")[1]
+    return token
+
+
+@protect.before_request
+def verify_token_middleware():
+    validate_token(token_info(), output=False)
+
+
+@protect.route("/index")
+def index():
+    try:
+        return jsonify({"Message": request.args.get('username')})
+    except Exception as error:
+        return jsonify({"Message": "Error"})
+
+
+@protect.route('/update')
+def update():
+    try:
+        return jsonify({"Message": "Welcome tu update"})
+    except Exception as error:
+        return jsonify({"Message": "Error"})
+
+
+@protect.route("/user")
+def verify():
+    try:
+        return jsonify({"User": validate_token(token_info(), output=True)})
+    except Exception as error:
+        return jsonify({"Message": "error"})
