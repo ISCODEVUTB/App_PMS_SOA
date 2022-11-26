034d555297dc651ac1ed16580e451a56eaaf6169
Esneider23 <98894964+Esneider23@users.noreply.github.com>
2022-11-26 09:28:25 -0500

    update login

--- Back-end/login/src/app.py
+++ Back-end/login/src/app.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
+from flask import Flask
 from database import mysql
 from routes.auth import routes_auth
 from routes.protect import protect
+from dotenv import load_dotenv
 
 server = Flask(__name__)
 server.config['MYSQL_HOST'] = 'us-cdbr-east-06.cleardb.net'
@@ -14,5 +15,5 @@ server.register_blueprint(protect, url_prefix="/api")
 
 
 if __name__ == '__main__':
-    server.secret_key = "KQSFKGNIJSOPDGBIOMOP"
+    load_dotenv()
     server.run(debug=True, port=3000, host="0.0.0.0")
--- Back-end/login/src/function_jwt.py
+++ Back-end/login/src/function_jwt.py
@@ -1,6 +1,7 @@
 from jwt import encode, decode, exceptions
 from flask import jsonify
 from datetime import datetime, timedelta
+from os import getenv
 
 
 def expire_date(day: int):
@@ -10,7 +11,7 @@ def expire_date(day: int):
 
 
 def write_token(data: dict):
-    token = encode(payload={**data, "exp": expire_date(2)}, key="KQSFKGNIJSOPDGBIOMOP", algorithm="HS256")
+    token = encode(payload={**data, "exp": expire_date(2)}, key=getenv("SECRET"), algorithm="HS256")
     token.encode("UTF-8")
     return token
 
@@ -18,9 +19,9 @@ def write_token(data: dict):
 def validate_token(token, output=False):
     try:
         if output:
-            return decode(token, key="KQSFKGNIJSOPDGBIOMOP", algorithms=["HS256"])
+            return decode(token, key=getenv("SECRET"), algorithms=["HS256"])
         else:
-            decode(token, key="KQSFKGNIJSOPDGBIOMOP", algorithms=["HS256"])
+            decode(token, key=getenv("SECRET"), algorithms=["HS256"])
     except exceptions.DecodeError:
         response = jsonify({"Message": "Invalid Token"})
         response.status_code = 401
