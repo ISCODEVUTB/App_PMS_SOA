98de90a3014044dee8dd790dc9f0bc90fb0e24c0
Esneider23 <98894964+Esneider23@users.noreply.github.com>
2022-11-26 15:34:42 -0500

    test

--- test.py
+++ test.py
@@ -4,9 +4,32 @@ import requests
 
 class ApiTest(unittest.TestCase):
     API_URL = "https://servicio-stock.onrender.com/"
-    URL = "{}/vehicles".format(API_URL)
+    vehicles = "{}/vehicles".format(API_URL)
+    vehicle = "{}/vehicle".format(API_URL)
+    stock = "{}/stock".format(API_URL)
+    vehicle_object = {
+            "name": "Onix Turbo RS 2023",
+            "motor": "MOTOR TURBO 115 HP TORQUE 160 NM",
+            "gearbox": "HP 115 Torque 160 Nm",
+            "security": "6 AIRBAGS",
+            "type": 4
+    }
+    stock_object = {
+        "name": 14,
+        "supplier": 1,
+        "selling_price": 1000000000,
+        "quantity": 2
+    }
 
     def test_1_get_all_vehicles(self):
-        r = requests.get(ApiTest.URL)
+        r = requests.get(ApiTest.vehicles)
+        self.assertEqual(r.status_code, 200)
+
+    def test_2_create_vehicle(self):
+        r = requests.post(ApiTest.vehicle, json=ApiTest.vehicle_object)
+        self.assertEqual(r.status_code, 200)
+
+    def test_3_create_stock(self):
+        r = requests.post(ApiTest.stock, json=ApiTest.stock_object)
         self.assertEqual(r.status_code, 200)
 
