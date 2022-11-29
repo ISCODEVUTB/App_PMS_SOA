0842fb2508ebf0283964bfc5b986bbf3889658cb
Esneider23 <98894964+Esneider23@users.noreply.github.com>
2022-11-29 16:18:50 -0500

    update

--- Back-end/users/test_controller.py
+++ Back-end/users/test_controller.py
@@ -1,8 +1,17 @@
 import unittest
+import controller
+import app
+
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def all_users(self):
+        r = controller.get_all_users(app.mysql)
+        self.assertEqual(len(r.json()), 2) # 2 users in the database
+
+    def user_by_id(self):
+        r = controller.user(app.mysql, 1)
+        self.assertEqual(r.json()['name'], 'Esneider Enrique') # user with id 1
+
 
 if __name__ == '__main__':
     unittest.main()
