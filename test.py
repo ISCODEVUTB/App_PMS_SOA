44b59539ee496d91a99b443bd1f433951614a5ca
Esneider23 <98894964+Esneider23@users.noreply.github.com>
2022-11-29 19:56:43 -0500

    test

--- test.py
+++ test.py
@@ -1,10 +1,7 @@
 from unittest import TestCase
-from Backend.users.app import all_users_1
-from app import suma
+from Backend.users.controller import all_users_1
+
 
 class TestSuma(TestCase):
     def test_suma(self):
         self.assertEqual(suma(1, 2), 3)
-
-    def test_all_users(self):
-        self.assertEqual(all_users_1(), 200)
