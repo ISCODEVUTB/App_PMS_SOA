9620c6cc9425563e4218d4f290aa336e0c375a8c
Esneider23 <98894964+Esneider23@users.noreply.github.com>
2022-12-19 20:27:29 -0500

    Update supplier_controller

--- supplier_contraller.py
+++ supplier_contraller.py
@@ -5,7 +5,8 @@ import json
 # This function returns the supplier information
 def show_supplier(mysql):
     cursor = mysql.cursor()
-    cursor.execute("select idsupplier, name, nit, address, phone, email from supplier")
+    cursor.execute(
+        "select idsupplier, name, nit, address, phone, email from supplier")
     data = cursor.fetchall()
     if data != None:
         list_supplier = []
@@ -15,29 +16,24 @@ def show_supplier(mysql):
             list_supplier.append(list_supplier_get)
             info = {'vehicles': list_supplier}
             return json.dumps(info)
-    else:
-        return json.dumps({'message': "There are no data"})
 
 
 # This function deletes a data from a supplier
 def delete_supplier(mysql, id_supplier):
-        cursor = mysql.cursor()
-        sql = "delete from supplier where idsupplier = '{0}'".format(id_supplier)
-        cursor.execute(sql)
-        mysql.commit()
-        info = {'status': 'deleted'}
-        return json.dumps(info)
+    cursor = mysql.cursor()
+    sql = "delete from supplier where idsupplier = '{0}'".format(id_supplier)
+    cursor.execute(sql)
+    mysql.commit()
+    info = {'status': 'deleted'}
+    return json.dumps(info)
 
 
 # This function updates a data of a supplier
 def update_supplier(mysql, id_supplier):
-    try:
-        cursor = mysql.connection.cursor()
-        cursor.execute("update supplier set name = '{0}', nit = '{1}', address = '{2}', phone = '{3}',"
-                       "email = '{4}' where idsupplier = '{5}'".format(request.json['name'], request.json['nit'],
-                                                                       request.json['address'], request.json['phone'],
-                                                                       request.json['email'], id_supplier))
-        mysql.connection.commit()
-        return show_supplier(mysql)
-    except ValueError:
-        return jsonify({'message: "Error'})
+    cursor = mysql.connection.cursor()
+    cursor.execute("update supplier set name = '{0}', nit = '{1}', address = '{2}', phone = '{3}',"
+                   "email = '{4}' where idsupplier = '{5}'".format(request.json['name'], request.json['nit'],
+                                                                   request.json['address'], request.json['phone'],
+                                                                   request.json['email'], id_supplier))
+    mysql.connection.commit()
+    return show_supplier(mysql)
